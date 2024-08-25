b7951ebecbbf7e15dfecd79b1c06777011c6acd0
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-08-25 20:59:48 +0200

    Update Tap.tsx

--- src/page/Tap.tsx
+++ src/page/Tap.tsx
@@ -12,14 +12,10 @@ import { useTonAddress } from "@tonconnect/ui-react";
 import { ToastContainer, toast } from "react-toastify";
 
 const Tap = () => {
-
-
-
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const address = useTonAddress();
 
   const [positionTop, setPositionTop] = useState(305);
-  // const [prevPositionTop, setPrevPositionTop] = useState(305);
   const [isGoing, setIsGoing] = useState(false);
   const [coins, setCoins] = useState([{ x: 850, y: 550, isGet: false }, { x: 100, y: 100, isGet: false }]);
   const [waterFall, setWaterFall] = useState([{ x: -200, y: 0 }, { x: 30, y: -60 }, { x: 250, y: -50 }]);
@@ -33,6 +29,7 @@ const Tap = () => {
   });
   const navigate = useNavigate();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [wallet, setWalletAddress] = useState("");
 
   useEffect(() => {
     if (spaceRef.current) {
@@ -47,42 +44,40 @@ const Tap = () => {
     const intervalId = setInterval(() => {
       cnt++;
       setCoins((prevCoins) => [
-        ...prevCoins.slice(0, 1), // Keep the first coin static
+        ...prevCoins.slice(0, 1),
         ...prevCoins.slice(1).map((coin) => ({ ...coin, x: coin.x - 5 })),
       ]);
       if (cnt > 10) {
         setCoins((prev) => [
           ...prev,
           getRandomPositions()
-        ])
+        ]);
 
         setWaterFall((prev) => {
-          if (prev.length >= 5) return prev; // Prevent adding more if limit is reached
+          if (prev.length >= 5) return prev;
           return [...prev, getRandomWaterFalls()];
         });
+
         if (waterFall.length > 5) {
-          const tmpFall = waterFall.filter((_fall, i) => { return i !== 3 });
+          const tmpFall = waterFall.filter((_fall, i) => i !== 3);
           setWaterFall(tmpFall);
         }
         cnt = 0;
       }
       setWaterFall((prev) => {
-        const tmp = prev.filter((fall, i) => {
-          return fall.x > -70 || i === 0;
-        });
+        const tmp = prev.filter((fall, i) => fall.x > -70 || i === 0);
         return [
           ...tmp.slice(0, 1),
           ...tmp.slice(1).map(fall => ({ ...fall, x: fall.x - 5 }))
-        ]
-      }
-      )
+        ];
+      });
+    }, 100);
 
-    }, 100); // Update positions every 100ms
-    return () => { clearInterval(intervalId); } // Clean up on unmount
+    return () => clearInterval(intervalId);
   }, [isStart]);
 
   const checkCollision = () => {
-    if (!spaceRef.current || !coins.length || !waterFall.length) return false;
+    if (!spaceRef.current || !coins.length) return false;
 
     const spaceshipRect = spaceRef.current.getBoundingClientRect();
     const spaceshipCenter = {
@@ -119,17 +114,17 @@ const Tap = () => {
   }
 
   const checkCollisionWithFall = async () => {
-    if (!spaceRef.current || !coins.length || !waterFall.length) return false; // Early exit if refs or coins array is empty
+    if (!spaceRef.current || !coins.length || !waterFall.length) return false;
     const spaceshipRect = spaceRef.current.getBoundingClientRect();
     const spaceXL = spaceshipRect.left;
     const spaceXR = spaceXL + spaceshipRect.width;
     const spaceYT = window.innerHeight - spaceshipRect.top - spaceshipRect.height;
 
-    waterFall.map(async (fall) => {
+    for (const fall of waterFall) {
       const fallXL = fall.x;
       const fallXR = fall.x + 70;
       const fallY = 400 + fall.y;
-      // console.log("+++++++++> ", fallY, spaceYT, window.innerHeight, spaceshipRect.top, spaceshipRect.height);
+
       if (
         ((fallXL <= spaceXR - 20) && (fallXL >= spaceXL))
         &&
@@ -144,25 +139,23 @@ const Tap = () => {
         console.log("-----------here", screen.height)
         await saveDataToDB();
         return navigate('/fail');
-        // alert("stop")
       }
-    });
-
-    return false; // No crash detected
+    }
+    return false;
   };
 
   const checkCrashWithEarth = () => {
-    if (!spaceRef.current) return false; // Ensure the spaceship ref is available
+    if (!spaceRef.current) return false;
     const spaceshipRect = spaceRef.current.getBoundingClientRect();
     const spaceshipLeft = window.scrollX + spaceshipRect.left;
     const spaceshipTop = window.scrollY + spaceshipRect.top;
     const earthCenterX = window.innerWidth / 2;
     const earthCenterY = window.innerHeight * 0.7 + 300;
-    const earthRadius = 300; // Half of the Earth's diameter
-  
+    const earthRadius = 300;
+
     const distanceFromCenterX = Math.abs(spaceshipLeft - earthCenterX);
     const distanceFromCenterY = Math.abs(spaceshipTop - earthCenterY);
-  
+
     if (distanceFromCenterX <= earthRadius && distanceFromCenterY <= earthRadius) {
       console.log("Spacecraft crashed into Earth!", earthCenterX, earthCenterY, window.innerHeight);
       saveDataToDB();
@@ -171,6 +164,7 @@ const Tap = () => {
     }
     return false;
   };
+
   useEffect(() => {
     if (isStart) {
       const intervalId = setInterval(() => {
@@ -178,6 +172,7 @@ const Tap = () => {
         checkCollisionWithFall();
         checkCrashWithEarth();
       }, 100);
+
       if (positionTop < -70 || window.innerHeight - positionTop - 106 < 0) {
         saveDataToDB();
         navigate('/fail');
@@ -190,29 +185,25 @@ const Tap = () => {
 
   async function fetchData(walletAddress: string) {
     try {
-      axios.post(`${backendUrl}/user/login`, {
+      const { data } = await axios.post(`${backendUrl}/user/login`, {
         wallet: walletAddress,
-      }).then(data => {
-        console.log("GetData: ", data.data.data);
-        setAccountInfo(data.data.data);
-        console.log("accountInfo: ", accountInfo)
-      })
+      });
+      setAccountInfo(data.data);
     } catch (error) {
       console.error("There was a problem with the fetch operation:", error);
       toast.error("Internal Server Error");
     }
   }
 
-  const [wallet, setWalletAddress] = useState("");
   useEffect(() => {
     if (address) {
       setWalletAddress(address);
-      fetchData(address)
+      fetchData(address);
     }
-  }, [address])
+  }, [address]);
 
   const getPositionOfSpaceship = () => {
-    if (!spaceRef.current) return; // Check if the ref is attached to an element
+    if (!spaceRef.current) return;
     const rect = spaceRef.current.getBoundingClientRect();
     const position = {
       x: window.scrollX + rect.left,
@@ -232,13 +223,14 @@ const Tap = () => {
     }
   };
 
-  const handleJump = () => {
+  const handleJump = (e) => {
+    e.preventDefault(); // Prevent default touch behavior
     if (!isStart) return;
     setIsGoing(true);
 
-    const jumpHeight = 60; // Define the total jump height
-    const jumpDuration = 500; // Define the duration of the jump in milliseconds
-    const jumpInterval = 10; // Define the interval for each increment in milliseconds
+    const jumpHeight = 60;
+    const jumpDuration = 500;
+    const jumpInterval = 10;
     const totalIncrements = jumpDuration / jumpInterval;
     const incrementAmount = jumpHeight / totalIncrements;
 
@@ -247,14 +239,13 @@ const Tap = () => {
     const jumpIntervalId = setInterval(() => {
       if (currentIncrement >= totalIncrements) {
         clearInterval(jumpIntervalId);
-        setIsGoing(false)
+        setIsGoing(false);
       } else {
         setPositionTop((prevPosition) => prevPosition - incrementAmount);
         currentIncrement++;
       }
     }, jumpInterval);
   };
-
 
   useEffect(() => {
     const images = [Coin, getCoinSVG, rotatedSpaceDown, rotatedSpaceUp, EarthImage, waterFallSVG];
@@ -271,24 +262,28 @@ const Tap = () => {
   }, []);
 
   if (!isLoaded) {
-    return (<div className='w-full h-full px-2 mt-auto'>Loading...</div>)
+    return (<div className='w-full h-full px-2 mt-auto'>Loading...</div>);
   }
 
   return (
-    <div className="w-full h-full flex flex-col overflow-hidden px-2" onClick={handleJump}>
+    <div
+      className="w-full h-full flex flex-col overflow-hidden px-2"
+      onTouchStart={handleJump} // Handle touch start
+    >
       <ToastContainer />
       <div className='bg-black my-4 mx-2 rounded-full ml-auto text-right flex flex-row gap-4 justify-between px-3 py-1 border-2 border-white items-center'>
-        <img src={Coin} alt='Coin'></img>
+        <img src={Coin} alt='Coin' />
         <div>{accountInfo.coin}</div>
       </div>
       <div className='text-center mt-[8%] text-black font-press-start z-10'>
-        <div className='text-[84px]' style={{ WebkitTextStrokeColor: 'white', WebkitTextStrokeWidth: 5 }}>{achievedCoin}</div>
+        <div className='text-[84px]' style={{ WebkitTextStrokeColor: 'white', WebkitTextStrokeWidth: 5 }}>
+          {achievedCoin}
+        </div>
       </div>
       <div>
         {coins.map((coin, index) => (
           !coin.isGet ? <img
             key={index}
-            // src={coin.isGet ? getCoinSVG : Coin}
             src={Coin}
             alt='Coin'
             className='blur-coin-outline-all'
@@ -300,12 +295,13 @@ const Tap = () => {
             }}
             width={52}
             height={69}
-          ></img> :
-            <div style={{
-              position: 'absolute',
-              left: `${coin.x}px`,
-              top: `${coin.y}px`
-            }}
+          /> :
+            <div
+              style={{
+                position: 'absolute',
+                left: `${coin.x}px`,
+                top: `${coin.y}px`
+              }}
               className='font-press-start text-[28px] animate-fadeouttopright transform'
               key={index}
               onLoad={() => {
@@ -314,14 +310,17 @@ const Tap = () => {
                   setCoins(tmp);
                 }, 1000);
               }}
-            >+1</div>
+            >
+              +1
+            </div>
         ))}
       </div>
-      <div ref={spaceRef}
+      <div
+        ref={spaceRef}
         className={`absolute top-[${positionTop}px] right-[50vw] flex justify-center items-center`}
-        onClick={() => { handleStart(), setStarted(true) }}
+        onTouchStart={() => { handleStart(); setStarted(true); }}
       >
-        <img src={isGoing ? rotatedSpaceUp : rotatedSpaceDown} alt='space-ship' width={100} height={61}></img>
+        <img src={isGoing ? rotatedSpaceUp : rotatedSpaceDown} alt='space-ship' width={100} height={61} />
       </div>
       <div className='flex'>
         {waterFall.map((fall, index) => (
@@ -330,14 +329,14 @@ const Tap = () => {
             alt='waterfall'
             key={index}
             className='h-[400px] w-[70px]'
-            style={{ position: 'absolute', left: `${fall.x}px`, bottom: `${fall.y}px`, }}
-          ></img>
+            style={{ position: 'absolute', left: `${fall.x}px`, bottom: `${fall.y}px` }}
+          />
         ))}
       </div>
       <div className='absolute bottom-[-400px] flex justify-center'>
         <img src={EarthImage} alt='earth' className='rotate-earth max-w-[150%] blur-outline-all'
           style={{ width: "600px", height: "600px" }}
-        ></img>
+        />
       </div>
     </div>
   );
