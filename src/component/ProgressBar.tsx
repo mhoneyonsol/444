c5b3bd95b6679818c54ea9f9a589c3691cdb67aa
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-08-25 19:58:19 +0200

    Revert "Update ProgressBar.tsx"
    This reverts commit cee538404421ee53537c9c6e450e251376f998c2.

--- src/component/ProgressBar.tsx
+++ src/component/ProgressBar.tsx
@@ -2,7 +2,7 @@ export default function ProgressBar({ value }: { value: number }) {
   return (
     <div className="flex items-center justify-center">
       <div
-        className="w-full h-[24px] border-[#1E3D4B] border-[2px] rounded-full flex items-center" style={{ marginTop: "-50px"}}
+        className="w-full h-[24px] border-[#1E3D4B] border-[2px] rounded-full flex items-center"
       >
         <div
           className="bg-gradient-to-r from-[#EF7300] to-[#711081] h-[14px] rounded-full mx-1"
